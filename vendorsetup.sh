4687e187fa56609398b17aadef38815f13b34c11
zaidkhan0997 <kzaid0997@gmail.com>
2023-06-11 13:50:08 +0530

    sweet: Switch to Sleepy kernel and Aosp r487747c 17.0.2 clang
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone -b 13 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git prebuilts/clang/host/linux-x86/r487747c
+git clone -b t13 --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
