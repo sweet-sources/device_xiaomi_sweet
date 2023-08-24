636f950bccca23c3bf24a4565de468936628b6fa
zaidkhan0997 <kzaid0997@gmail.com>
2023-08-25 01:55:38 +0530

    sweet: sync kernel & clang from vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,3 @@
 git clone -b 13 https://github.com/sweet-sources/vendor_dolby.git vendor/dolby
+git clone -b 13 --depth=1 https://github.com/sweet-sources/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git prebuilts/clang/host/linux-x86/r487747c
