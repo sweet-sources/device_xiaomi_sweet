fd8b39d92f301e4afae8024b499471d2cf3cb45c
zaidkhan0997 <kzaid0997@gmail.com>
2023-10-26 00:37:04 +0530

    sweet: add vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone -b 14 --depth=1 https://github.com/sweet-sources/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git prebuilts/clang/host/linux-x86/r498229b
+git clone -b lineage-21.0 https://github.com/LineageOS/android_hardware_lineage_compat.git hardware/lineage/compat
